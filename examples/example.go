07b78bc2da000ad6f47e596df8fbbdf5356905af
zc <zc2638@qq.com>
2021-09-18 15:39:00 +0800

    add examples

---
+++ examples/example.go
@@ -0,0 +1,47 @@
+// Package examples
+// Created by zc on 2021/9/18.
+package main
+
+import (
+	"context"
+
+	"github.com/zc2638/aide"
+)
+
+func main() {
+	first := aide.NewStage("first").AddSteps(
+		check().Step("check"),
+		check().Step("check"),
+		check().Step("check"),
+		check().Step("check"),
+	)
+	second := aide.NewStage("second").AddSteps(
+		install().Step("install"),
+	)
+	third := aide.NewStage("third").AddSteps(
+		health().Step("health check"),
+	)
+
+	a := aide.New()
+	a.AddStages(first, second, third)
+	_ = a.Run(context.Background())
+}
+
+func check() aide.StepResultFunc {
+	return func(ctx *aide.StepContext) {
+		ctx.WriteString("check Port 31181 OK.")
+	}
+}
+
+func install() aide.StepResultFunc {
+	return func(ctx *aide.StepContext) {
+		ctx.WriteString("Install Component Successful.")
+	}
+}
+
+func health() aide.StepResultFunc {
+	return func(ctx *aide.StepContext) {
+		ctx.Exit(1)
+		ctx.WriteString("component unhealthy.")
+	}
+}
